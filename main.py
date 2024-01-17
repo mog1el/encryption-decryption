3124c7bf910e74077f0d96bae86ee8e73c19fede
mog1el <74179033+mog1el@users.noreply.github.com>
2024-01-17 20:48:08 +0100

    Adding the rest
    Added decryption and changed the long boring lines of code to dictionaries. Will add special characters in the future

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import random
 
+def xor_strings(str1, str2):
+    return ''.join(str(int(bit1) ^ int(bit2)) for bit1, bit2 in zip(str1, str2))
+
 key = open("key.txt", "a")
 if os.stat("key.txt").st_size == 0:
     if_key = input("Looks like you don't have a key. Should I generate a new one? (Y/N)")
@@ -19,7 +22,6 @@ with open("key.txt", 'rb') as file:
         binary_representation = bin(byte[0])[2:]
         bina.append(binary_representation)
         byte = file.read(1)
-print(bina)
 steps = 1
 encrypted_message = []
 if os.stat("encrypted.txt").st_size == 0:
@@ -27,70 +29,51 @@ if os.stat("encrypted.txt").st_size == 0:
     print("What message would you like to encrypt? (without numbers and special characters)")
     message_encryption = str(input())
     #in the future I'm going to make this encryption more random by including in the key something to automate translating letters to numbers
-    for i in range(len(message_encryption)):
-        if message_encryption[i].lower() == "a":
-            encrypted_message.append("7")
-        elif message_encryption[i].lower() == "b":
-            encrypted_message.append("14")
-        elif message_encryption[i].lower() == "c":
-            encrypted_message.append("5")
-        elif message_encryption[i].lower() == "d":
-            encrypted_message.append("13")
-        elif message_encryption[i].lower() == "e":
-            encrypted_message.append("20")
-        elif message_encryption[i].lower() == "f":
-            encrypted_message.append("22")
-        elif message_encryption[i].lower() == "g":
-            encrypted_message.append("12")
-        elif message_encryption[i].lower() == "h":
-            encrypted_message.append("21")
-        elif message_encryption[i].lower() == "i":
-            encrypted_message.append("24")
-        elif message_encryption[i].lower() == "j":
-            encrypted_message.append("6")
-        elif message_encryption[i].lower() == "k":
-            encrypted_message.append("23")
-        elif message_encryption[i].lower() == "l":
-            encrypted_message.append("25")
-        elif message_encryption[i].lower() == "m":
-            encrypted_message.append("18")
-        elif message_encryption[i].lower() == "n":
-            encrypted_message.append("10")
-        elif message_encryption[i].lower() == "o":
-            encrypted_message.append("11")
-        elif message_encryption[i].lower() == "p":
-            encrypted_message.append("3")
-        elif message_encryption[i].lower() == "q":
-            encrypted_message.append("9")
-        elif message_encryption[i].lower() == "r":
-            encrypted_message.append("8")
-        elif message_encryption[i].lower() == "s":
-            encrypted_message.append("15")
-        elif message_encryption[i].lower() == "t":
-            encrypted_message.append("26")
-        elif message_encryption[i].lower() == "u":
-            encrypted_message.append("19")
-        elif message_encryption[i].lower() == "v":
-            encrypted_message.append("2")
-        elif message_encryption[i].lower() == "w":
-            encrypted_message.append("16")
-        elif message_encryption[i].lower() == "x":
-            encrypted_message.append("17")
-        elif message_encryption[i].lower() == "y":
-            encrypted_message.append("4")
-        elif message_encryption[i].lower() == "z":
-            encrypted_message.append("1")
+    char_to_number = {
+        'a': '7', 'b': '14', 'c': '5', 'd': '13', 'e': '20',
+        'f': '22', 'g': '12', 'h': '21', 'i': '24', 'j': '6',
+        'k': '23', 'l': '25', 'm': '18', 'n': '10', 'o': '11',
+        'p': '3', 'q': '9', 'r': '8', 's': '15', 't': '26',
+        'u': '19', 'v': '2', 'w': '16', 'x': '17', 'y': '4',
+        'z': '1'
+    }
+    for char in message_encryption:
+        encrypted_message.append(char_to_number[char.lower()])
     encrypted_message = [bin(int(x))[2:] for x in encrypted_message]
     encrypted_message = [s.zfill(6) for s in encrypted_message]
-    def xor_strings(str1, str2):
-        return ''.join(str(int(bit1) ^ int(bit2)) for bit1, bit2 in zip(str1, str2))
     for i in range (len(encrypted_message)):
         step = i%4
         result_list = [xor_strings(s1, s2) for s1, s2 in zip(encrypted_message[i], bina[step])]
-        print(bina[step])
         encrypted_message[i] = ''.join(result_list)
     for i in range (len(encrypted_message)):
         encrypted_message[i] = int(encrypted_message[i], 2)
     with open("encrypted.txt", "a") as file:
         encrypted_message = ''.join(map(str, encrypted_message))
         file.write(encrypted_message)
+else:
+    print("Decrypting message...")
+    decrypted_message = []
+    message_decryption = []
+    with open("encrypted.txt", "r") as file:
+        file_content = file.read().strip()
+    message_decryption = [int(file_content[i:i+2]) for i in range(0, len(file_content), 2)]
+    message_decryption = [bin(int(x))[2:] for x in message_decryption]
+    for i in range (len(message_decryption)):
+        step = i%4
+        result_list = [xor_strings(s1, s2) for s1, s2 in zip(message_decryption[i], bina[step])]
+        message_decryption[i] = ''.join(result_list)
+    for i in range (len(message_decryption)):
+        message_decryption[i] = int(message_decryption[i], 2)
+    number_to_char = {
+        '7': 'a', '14': 'b', '5': 'c', '13': 'd', '20': 'e',
+        '22': 'f', '12': 'g', '21': 'h', '24': 'i', '6': 'j',
+        '23': 'k', '25': 'l', '18': 'm', '10': 'n', '11': 'o',
+        '3': 'p', '9': 'q', '8': 'r', '15': 's', '26': 't',
+        '19': 'u', '2': 'v', '16': 'w', '17': 'x', '4': 'y',
+        '1': 'z'
+    }
+    for number in message_decryption:
+        if str(number) in number_to_char:
+            decrypted_message.append(number_to_char[str(number)])
+    decrypted_message = ''.join(decrypted_message)
+    print("Decrytpted message is:", decrypted_message)
